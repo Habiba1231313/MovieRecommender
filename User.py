f40068e0639a28d81a02d973c77b0396871ba37e
Habiba1231313 <65048401+Habiba1231313@users.noreply.github.com>
2025-08-11 12:56:43 +0300

    Update User.py

--- User.py
+++ User.py
@@ -7,16 +7,3 @@ class user:
         with open(self.file_name, 'a') as file:
             file.write(f"{movie['title']} ({movie['release_date']}) - {movie['overview']}\n")
         print(f"Movie '{movie['title']}' saved to {self.file_name}.")
-
-    def view_watchlist(self):
-        try:
-            with open(self.file_name, 'r') as file:
-                watchlist = file.readlines()
-                if watchlist:
-                    print(f"Watchlist for {self.name}:")
-                    for movie in watchlist:
-                        print(movie.strip())
-                else:
-                    print("Your watchlist is empty.")
-        except FileNotFoundError:
-            print("Watchlist file not found. Please save a movie first.") 
